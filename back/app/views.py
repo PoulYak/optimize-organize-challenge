8d9b30e51acc7d8573a58a160eb30ea2be61cebb
lokot0k <kaptoxa322@mail.ru>
2023-04-07 21:45:32 +0300

    login

--- back/app/views.py
+++ back/app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http import HttpRequest, JsonResponse
+
+
+def login_user(request: HttpRequest):
+    if request.method != "POST":
+        return None
+    credentials = json.loads(request.body)
+    user = authenticate(request, email=credentials['login'],
+                        password=credentials['password'])
+    if user is not None:
+        login(request, user)
+        return JsonResponse({'success': "true"})
+    else:
+        return JsonResponse(
+            {'success': "false", 'message': "Invalid login or password"},
+            status=400)
