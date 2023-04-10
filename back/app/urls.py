a0a6d3c4feee0b9c19d3f7895de2d0df16461ed4
lokot0k <kaptoxa322@mail.ru>
2023-04-10 04:05:01 +0300

    tag delete endpoint

--- back/app/urls.py
+++ back/app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('logout/', user.logout_user, name="logout"),
     path('role/', user.get_role, name='get_role'),
     path('user/', user.get_user, name='get_user'),
-    path('tags/', tag.TagView.as_view(), name='crud_tags'),
+    path('tags/', tag.TagListView.as_view(), name='crud_tags'),
     path('facilities/', facility.FacilityView.as_view(),
          name='crud_facilities'),
     path('solutions/', solution.SolutionView.as_view(), name="create_solution"),
@@ -16,4 +16,5 @@ urlpatterns = [
     path('report/', misc.get_report, name="get_report"),
     path('load_xml/', misc.create_from_xml, name="create_from_xml"),
     path('media/', media.MediaView.as_view(), name="create_media"),
+    path('tags/<int:id>/', tag.TagView.as_view(), name="delete_tag"),
 ]
--- back/app/views/tag.py
+++ back/app/views/tag.py
@@ -8,7 +8,7 @@ from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class TagView(LoginRequiredMixin, View):
+class TagListView(LoginRequiredMixin, View):
     def get(self, request: HttpRequest, *args,
             **kwargs) -> JsonResponse | HttpResponse:
         query = tag.all()
@@ -16,6 +16,31 @@ class TagView(LoginRequiredMixin, View):
         tags = serializer.serialize(query)
         res = '{"success": "true", "tags": ' + tags + "}"
         return HttpResponse(res, content_type='application/json')
+
+    def post(self, request: HttpRequest, *args, **kwargs) -> JsonResponse:
+        body = json.loads(request.body)
+        tag.create(**body)
+        return JsonResponse(
+            {'success': 'true', 'message': 'tag created'})
+
+    def head(self, request, *args, **kwargs):
+        return JsonResponse(
+            {'success': 'false', 'message': 'unsupported method'}, status=403)
+
+    def put(self, request, *args, **kwargs):
+        return JsonResponse(
+            {'success': 'false', 'message': 'unsupported method'}, status=403)
+
+
+class TagView(LoginRequiredMixin, View):
+    def delete(self, request: HttpRequest, id):
+        tag.delete(id)
+        return JsonResponse({'success': 'true'})
+
+    def get(self, request: HttpRequest, *args,
+            **kwargs) -> JsonResponse | HttpResponse:
+        return JsonResponse(
+            {'success': 'false', 'message': 'unsupported method'}, status=403)
 
     def post(self, request: HttpRequest, *args, **kwargs) -> JsonResponse:
         body = json.loads(request.body)
